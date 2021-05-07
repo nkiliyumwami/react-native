174fbe5b2482459d6f67aa0ac1abe3d3c185e943
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-05-06 21:02:01 -0400

    Components Part 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { StyleSheet} from 'react-native';
 
 function Directory(props) {
 
     const renderDirectoryItem = ({item}) => {
         return (
-            <ListItem
+            <ListItem 
                 title={item.name}
                 subtitle={item.description}
+                onPress={() => props.onPress(item.id)}
                 leftAvatar={{ source: require('./images/react-lake.jpg')}}
+                
             />
         );
     };
 
     return (
-        <FlatList
+        <FlatList 
             data={props.campsites}
             renderItem={renderDirectoryItem}
             keyExtractor={item => item.id.toString()}
+            
         />
     );
 }
+
+
 
 export default Directory;
