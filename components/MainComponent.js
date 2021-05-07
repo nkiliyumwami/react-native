bfda2028bab576d289f7ea6b4d12c192e93af4a4
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-05-07 11:49:50 -0400

    Navigation Part 3

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+import {SafeAreaView} from 'react-native'
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -58,12 +59,12 @@ const AppNavigator = createAppContainer(MainNavigator)
 class Main extends Component {
     render() {
         return (
-            <View style={{
+            <SafeAreaView style={{
                 flex: 1,
                 paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
             }}>
                 <AppNavigator />
-            </View>
+            </SafeAreaView>
         );
     }
 }
