5454bd00ea5edd6c06af17067e0124f213631702
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-05-07 22:31:44 -0400

    Assignement Week 1

---
+++ components/ContactComponent.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { Text } from 'react-native';
+
+ class Contact extends Component {
+
+    static navigationOptions = {
+        title:'Contact Us'
+    };
+
+    render() {
+        return (
+            <ScrollView>
+                <Card title='Contact Information' wrapperStyle={{margin: 20}}>
+                    <Text style={{marginBottom: 10}}> 
+                            1 Nucamp Way
+                            Seattle, WA 98001
+                            U.S.A.
+                    </Text>
+                    <Text >Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
+
+                </Card>
+            </ScrollView>
+        )
+    }
+}
+
+export default Contact;
