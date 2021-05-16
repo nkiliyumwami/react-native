a7e1ae9766c3e53b5f45a931d669c1e3f84c9b86
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-05-16 19:50:34 -0400

    Icons, Favorites and Comments

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites'
+import { Text, View,ScrollView, FlatList } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
 
-function RenderCampsite({campsite}) {
+function RenderCampsite(props) {
+    //Destructring the campsite props from the entre props en haut 
+    const {campsite} = props;
+
     if(campsite) {
         return(
             <Card
@@ -12,6 +16,16 @@ function RenderCampsite({campsite}) {
                 <Text style={{margin:10}} >
                   {campsite.description}  
                 </Text>
+                <Icon
+                   name={props.favorite ? 'heart' : 'heart-o' }
+                   type='font-awesome'
+                   color='#f50'
+                   raised
+                   reverse
+                   //Pass the onpress and make sure you can press by just once
+                   onPress={() => props.favorite ? console.log('Already set as a favorite') : props.markFavorite()}
+
+                />
             </Card>
         );
     }
@@ -20,13 +34,41 @@ function RenderCampsite({campsite}) {
     )
 }
 
+function RenderComments({comments}) {
+
+    const renderCommentItem = ({item}) => {
+        return(
+            <View style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.text}</Text>
+                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+                <Text style={{fontSize: 12}}>{`--${item.author}, ${item.date}`}</Text>
+            </View>
+        );
+    }
+    return(
+        <Card title='Comments'>
+            <FlatList
+                data={comments}
+                renderItem={renderCommentItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        </Card>
+    )
+}
  class CampsiteInfo extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS,
+            favorite: false
         };
+    }
+
+    //Event handler to toggle the favorite icon to true
+    markFavorite() {
+        this.setState({favorite: true});
     }
 
     //Set the title for the screen
@@ -38,9 +80,18 @@ function RenderCampsite({campsite}) {
         //Create an array of object with props naviation then filter it to get each campsiteId 
         const campsiteId = this.props.navigation.getParam('campsiteId');
         const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
+        //Filter out (into a new array called 'comments') only the comments for a particular campsite using campsiteId
+        const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId);
         return (
-        <RenderCampsite campsite={campsite} />
-    )
+            <ScrollView>
+                <RenderCampsite campsite={campsite} 
+                    favorite={this.state.favorite}
+                    markFavorite={() => this.markFavorite()}
+                />
+                <RenderComments comments={comments} />
+            </ScrollView>
+        
+    );
     }
     
 }
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -15,11 +15,9 @@ import { Text } from 'react-native';
         return (
             <ScrollView>
                 <Card title='Contact Information' wrapperStyle={{margin: 20}}>
-                    <Text style={{marginBottom: 10}}> 
-                            1 Nucamp Way
-                            Seattle, WA 98001
-                            U.S.A.
-                    </Text>
+                    <Text>1 Nucamp Way</Text> 
+                    <Text >Seatle, WA 98001</Text>
+                    <Text style={{marginBottom: 10}} >U.S.A</Text>
                     <Text >Phone: 1-206-555-1234</Text>
                     <Text>Email: campsites@nucamp.co</Text>
 
