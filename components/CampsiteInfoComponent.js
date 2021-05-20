544f41e496af8eb4a924d11be2e75be07b70600e
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-05-19 20:47:51 -0400

    Redux Adding Favorites

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -3,12 +3,18 @@ import { Text, View,ScrollView, FlatList } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
         campsites: state.campsites,
-        comments: state.comments
+        comments: state.comments,
+        favorites: state.favorites
     };
+};
+
+const mapDispatchToProps = {
+    postFavorite: campsiteId => (postFavorite(campsiteId))
 };
 
 function RenderCampsite(props) {
@@ -64,16 +70,11 @@ function RenderComments({comments}) {
 }
  class CampsiteInfo extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            favorite: false
-        };
-    }
+    
 
     //Event handler to toggle the favorite icon to true
-    markFavorite() {
-        this.setState({favorite: true});
+    markFavorite(campsiteId) {
+        this.props.postFavorite(campsiteId);
     }
 
     //Set the title for the screen
@@ -90,8 +91,8 @@ function RenderComments({comments}) {
         return (
             <ScrollView>
                 <RenderCampsite campsite={campsite} 
-                    favorite={this.state.favorite}
-                    markFavorite={() => this.markFavorite()}
+                    favorite={this.props.favorites.includes(campsiteId)}
+                    markFavorite={() => this.markFavorite(campsiteId)}
                 />
                 <RenderComments comments={comments} />
             </ScrollView>
@@ -101,4 +102,4 @@ function RenderComments({comments}) {
     
 }
 
-export default connect(mapStateToProps) (CampsiteInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo);
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { SafeAreaView } from 'react-native';
 import { View, Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -13,7 +13,20 @@ const mapStateToProps = state => {
     };
 };
 //Create a function/Component to render featured items into cards
-function RenderItem({item}) {
+function RenderItem(props) {
+    const {item} = props;
+
+     if (props.isLoading) {
+        return <Loading />;
+    }
+    if (props.errMess) {
+        return (
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    }
+
     if(item) {
         return (
             <Card
@@ -38,9 +51,21 @@ class Home extends Component {
     render() {
         return (
             <ScrollView>
-                <RenderItem item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]} />
-                <RenderItem item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]} />
-                <RenderItem item={this.props.partners.partners.filter(partner => partner.featured)[0]} />
+                <RenderItem 
+                    item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+                    isLoading={this.props.campsites.isLoading}
+                    errMess={this.props.campsites.errMess} 
+                />
+                <RenderItem 
+                    item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]} 
+                    isLoading={this.props.promotions.isLoading}
+                    errMess={this.props.promotions.errMess}
+                />
+                <RenderItem 
+                    item={this.props.partners.partners.filter(partner => partner.featured)[0]} 
+                    isLoading={this.props.partners.isLoading}
+                    errMess={this.props.partners.errMess}
+                />
             </ScrollView>
         );
     }
