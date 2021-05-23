fdcb1bf6681962d306e45272d21c039c459e7cba
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-05-22 21:37:41 -0400

    Animatable

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postComment, postFavorite } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -28,7 +29,8 @@ function RenderCampsite(props) {
 
     if(campsite) {
         return(
-            <Card
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card
                 featuredTitle={campsite.name}
                 image={{uri: baseUrl + campsite.image}}>
                 <Text style={{margin:10}} >
@@ -54,9 +56,9 @@ function RenderCampsite(props) {
                     onPress={() => props.onShowModal()}
                 />
                 </View>
-                
-                
             </Card>
+            </Animatable.View>
+            
         );
     }
     return (
@@ -80,13 +82,15 @@ function RenderComments({comments}) {
         );
     }
     return(
-        <Card title='Comments'>
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </Card>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title='Comments'>
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Card>
+        </Animatable.View>
     )
 }
  class CampsiteInfo extends Component {
